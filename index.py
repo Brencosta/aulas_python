d07c08700616735dce54c201ab89a2500102114c
Breno cunha <brenocunha977@gmail.com>
2026-06-15 15:54:22 -0300

    wertyui

---
+++ index.py
@@ -0,0 +1,8 @@
+print("hello world!!")
+
+nome = input("Digite seu nome: ")
+print("Ola", nome)
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+soma = num1 + num2
+print("A soma dos numeros é:", soma)
