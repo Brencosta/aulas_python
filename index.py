d12f5f51b628c9f9a681ad199b2d281f7ba4ba9e
Breno cunha <brenocunha977@gmail.com>
2026-06-18 10:19:19 -0300

    criei um sisetma de nome e idade

--- index.py
+++ index.py
@@ -1,8 +1,4 @@
-print("hello world!!")
-
 nome = input("Digite seu nome: ")
-print("Ola", nome)
-num1 = int(input("Digite um numero: "))
-num2 = int(input("Digite outro numero: "))
-soma = num1 + num2
-print("A soma dos numeros é:", soma)
+print(f"Olá, {nome}! Bem-vindo ao Python!")
+idade = int(input("Digite sua idade: "))
+print(f"Você tem {idade} anos.")
